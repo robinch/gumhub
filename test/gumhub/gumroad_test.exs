140b36c9815b8016a3f980dff00e7837e714178d
Robin Chowdhury <robinch88@gmail.com>
2023-03-02 18:37:35 +0100

    capture error log during test

--- test/gumhub/gumroad_test.exs
+++ test/gumhub/gumroad_test.exs
@@ -1,6 +1,8 @@
 defmodule GumHub.GumroadTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureLog
+
   alias GumHub.Gumroad
   alias GumHub.Fixtures
 
@@ -19,7 +21,12 @@ defmodule GumHub.GumroadTest do
         Fixtures.Gumroad.successful_verify_license(2)
     end)
 
-    assert {:error, :too_many_uses} == Gumroad.verify_license("product_id", "license_id")
+    log =
+      capture_log(fn ->
+        assert {:error, :too_many_uses} == Gumroad.verify_license("product_id", "license_id")
+      end)
+
+    assert log =~ "[error] Too many uses (2) on license (license_id)"
   end
 
   test "error: not verified" do
@@ -28,6 +35,11 @@ defmodule GumHub.GumroadTest do
         Fixtures.Gumroad.license_not_foud()
     end)
 
-    assert {:error, :license_not_found} = Gumroad.verify_license("product_id", "license_id")
+    log =
+      capture_log(fn ->
+        assert {:error, :license_not_found} = Gumroad.verify_license("product_id", "license_id")
+      end)
+
+    assert log =~ "[error] License (license_id) not found"
   end
 end
